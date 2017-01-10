413cf70dd5d52c0d538c58f582202e9e4f6f2b39
pranjal <pranjalkumar8228@gmail.com>
2017-01-11 03:19:59 +0530

    updates

--- index.php
+++ index.php
@@ -1,25 +1,39 @@
 <?php
-if(isset($_POST['addr'])&& !empty($_POST['addr']))
+if(isset($_POST['addr'])&& !empty($_POST['addr'])&& isset($_POST['task'])&& !empty($_POST['task']))
 {
     $url= $_POST['addr'];
 
     $arr = array();
-
+    $i=0;
     if (is_dir($url))
     {
         if ($handle = opendir($url))
         {
             while(($file = readdir($handle)) !== FALSE)
             {
-                $arr[] = $file;
+                $arr[] = $file;$i++;
             }
         }
     }
+    $match=0;
+    for($j=0;$j<$i;$j++)
+    {
+        for($k=$j+1;$k<$i;$k++)
+        {
+            if(((string)$arr[$j] )==((string)$arr[$k]))
+            {   echo $arr[$k];
+                $match++;
+            }
+        }
+    }
+    if($match==0)
+        echo "No files are present in the directory with the same name";
 
     closedir($handle);
 }
-echo "Please fill the details first";
-?>
+else {
+    echo "Please fill the details first";
+}?>
 
 <!DOCTYPE HTML>
 <html>
@@ -33,6 +47,8 @@ echo "Please fill the details first";
 <div id="form">
     <form action="index.php" method="post">
         <input type="text" name="addr" id="addr">
+        <input type="radio" name="task" value="task">View the List of Files with the same name
+        <input type="radio" name="task" value="task">Delete the files with the same name
         <button type="submit">Submit</button>
     </form>
 </div>
