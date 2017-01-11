1599223a8ed97bd8c846d64dae55c1ed4a753c6d
pranjal <pranjalkumar8228@gmail.com>
2017-01-11 10:12:49 +0530

    y

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-if(isset($_POST['addr'])&& !empty($_POST['addr'])&& isset($_POST['task'])&& !empty($_POST['task']))
+if(isset($_POST['addr'])&& !empty($_POST['addr'])&& !empty($_POST['task']))
 {
     $url= $_POST['addr'];
+    $task=$_POST['task'];
 
     $arr = array();
     $i=0;
@@ -15,14 +16,23 @@ if(isset($_POST['addr'])&& !empty($_POST['addr'])&& isset($_POST['task'])&& !emp
             }
         }
     }
+
     $match=0;
     for($j=0;$j<$i;$j++)
     {
         for($k=$j+1;$k<$i;$k++)
         {
             if(((string)$arr[$j] )==((string)$arr[$k]))
-            {   echo $arr[$k];
-                $match++;
+            {
+//            {   echo $arr[$k];
+//                $match++;
+                if($task=='see'){
+                    echo $arr[$k];
+                }
+                if($task=='delete')
+                {$add=$url.'/'.$arr[$k];
+                unlink($addr);}
+
             }
         }
     }
@@ -47,8 +57,8 @@ else {
 <div id="form">
     <form action="index.php" method="post">
         <input type="text" name="addr" id="addr">
-        <input type="radio" name="task" value="task">View the List of Files with the same name
-        <input type="radio" name="task" value="task">Delete the files with the same name
+        <input type="radio" name="task" value="see">View the List of Files with the same name
+        <input type="radio" name="task" value="delete">Delete the files with the same name
         <button type="submit">Submit</button>
     </form>
 </div>
